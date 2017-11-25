9d0b279c43191742bc99d4255f4eedbcc7b09ccd
SCUDRT <1208044562@qq.com>
2017-11-25 23:06:52 +0800

    Update 1174.cpp

--- LV0/1174.cpp
+++ LV0/1174.cpp
@@ -1,12 +1,9 @@
-//practising RMQ
 #include <cstdio>
 #include <cmath>
-int f[10001][16],n,q;
+int f[10001][14],n,q,a,b;
 inline int max(int a,int b)
 {
-	if (a>b)
-		return a;
-	return b;
+	return a>b?a:b;
 }
 void preDeal()
 {
@@ -14,13 +11,11 @@ void preDeal()
 		for (int i=1;i+(1<<k)-1<=n;++i)
 			f[i][k]=max(f[i][k-1],f[i+(1<<(k-1))][k-1]);
 }
-int RMQ(int from,int to)
+inline int RMQ()
 {
-	int temp=to-from+1,x=temp&(-temp);
-	if (x==temp)
-		return f[from][(int)log2(x)];
-	else
-		return max(f[from][(int)log2(x)],RMQ(from+x,to));
+	++a;++b;
+	int x=(int)log2(b-a+1);
+	return max(f[a][x],f[b-(1<<x)+1][x]);
 }
 int main()
 {
@@ -31,9 +26,8 @@ int main()
 	scanf("%d",&q);
 	while (q--)
 	{
-		int a,b;
 		scanf("%d%d",&a,&b);
-		printf("%d\n",RMQ(a+1,b+1));
+		printf("%d\n",RMQ());
 	}
 	return 0;
 }

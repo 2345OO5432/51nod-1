0be43a9966096715f84edd63d57afe2a95011046
SCUDRT <1208044562@qq.com>
2017-12-09 22:30:02 +0800

    Update 1103.cpp

--- LV4/1103.cpp
+++ LV4/1103.cpp
@@ -1,42 +1,28 @@
 #include <cstdio>
-int n,a,b,all,head[50001],next[50001],v[50001];
-long long s[50001];
-void add(int x,int pos)
-{
-    next[++all]=head[x];
-    head[x]=all;
-    v[all]=pos;
-}
-void deal()
-{
-    if (head[0])
-    {
-        a=0;
-        b=v[head[0]];
-        return;
-    }
-    for (int i=1;i<=n;++i)
-    {
-        if (next[head[i]])
-        {
-            a=v[next[head[i]]];
-            b=v[head[i]];
-            return;
-        }
-    }
-}
+int n,sum,a[50001],b[50001];
 int main()
 {
     scanf("%d",&n);
     for (int i=1;i<=n;++i)
     {
-        scanf("%lld",&s[i]);
-        s[i]+=s[i-1];
-        add(s[i]%n,i);
+        scanf("%d",&a[i]);
+        sum=(sum+a[i])%n;
+        if (!sum)
+        {
+            printf("%d\n",i);
+            for (int j=1;j<=i;++j)
+                printf("%d\n",a[j]);
+            break;
+        }
+        else if (b[sum])
+        {
+            printf("%d\n",i-b[sum]);
+            for (int j=b[sum]+1;j<=i;++j)
+                printf("%d\n",a[j]);
+            break;
+        }
+        else
+            b[sum]=i;
     }
-    deal();
-    printf("%d\n",b-a);
-    for (int i=a+1;i<=b;++i)
-        printf("%lld\n",s[i]-s[i-1]);
     return 0;
 }

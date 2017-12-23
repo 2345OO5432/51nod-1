754ec5675624858609ccc2a347baccb84da673cb
SCUDRT <1208044562@qq.com>
2017-12-23 21:26:21 +0800

    Update 1246.cpp

--- LV3/1246.cpp
+++ LV3/1246.cpp
@@ -1,14 +1,2 @@
-#include <cstdio>
-int n,k,c,ans;
-int main()
-{
-    scanf("%d%d%d",&n,&k,&c);
-    if (k<n)
-        ans=n-k+c;
-    else if (k%n==0)
-        ans=c;
-    else
-        ans=(c>k/n*n?c+n-k/(k/n+1):c);
-    printf("%d\n",ans);
-    return 0;
-}
+#include<stdio.h>
+main(){int n,k,c;scanf("%d%d%d",&n,&k,&c);printf("%d\n",c>k/n*n?c+n-k/(k/n+1):c);}
